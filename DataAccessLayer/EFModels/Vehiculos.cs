9742b1ca197f746544e017d328e3c462ea957d3a
Andres.Castro <andres.castro@estudiantes.utec.edu.uy>
2023-09-10 13:43:14 -0300

    Entrega

---
+++ DataAccessLayer/EFModels/Vehiculos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EFModels
+{
+    internal class Vehiculos
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public long Id { get; set; }
+        [MaxLength(128), MinLength(3), Required]
+        public string Marca { get; set; } = "-- Sin Marca --";
+        [MaxLength(128), MinLength(3), Required]
+        public string Modelo { get; set; } = "-- Sin Modelo --";
+        [MaxLength(128), MinLength(3), Required]
+        public string Matricula { get; set; } = "-- Sin Matricula --";
+        public Personas Persona { get; set; } = new Personas();
+
+
+    }
+}
---
+++ Shared/Vehiculo.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Shared
+{
+    public class Vehiculo
+    {
+        public string Marca { get; set; } = "-- Sin Marca --";
+        public string Modelo { get; set; } = "-- Sin Modelo --";
+        public string Matricula { get; set; } = "-- Sin Matriculo --";
+        public Vehiculo() { }
+    }
+}
